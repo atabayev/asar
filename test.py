3404abff6cf047ee375244e66990217d53eeaeea
Yel <atabayeb.yeldos@gmail.com>
2019-01-23 18:06:53 +0600

    2301-1806

--- test.py
+++ test.py
@@ -1,5 +1,27 @@
-from manager.models.Stack import Stack
+import json
+import os
+import requests
+from time import sleep
 
-stack = Stack.objects.filter(status=1).distinct()
-for st in stack:
-    print(st.email)
+
+def check_ip():
+    result = True
+    if json.loads(requests.get('https://www.iplocate.io/api/lookup/' +
+                               requests.get('https://api.ipify.org').text).text)['country_code'] == 'KZ':
+        return result
+    else:
+        result = False
+        try:
+            config_files = os.listdir('VPN_configs')
+        except FileNotFoundError:
+            print('check_ip', 'Нет файлов конфигураций для VPN')
+            return False
+        for config_file in config_files:
+            script = 'openvpn --configure {0}'.format(config_file)
+            os.system("bash -c '%s'" % script)
+            sleep(15)
+            if json.loads(requests.get('https://www.iplocate.io/api/lookup/' +
+                                       requests.get('https://api.ipify.org').text).text)['country_code'] == 'KZ':
+                result = True
+                break
+        return result
